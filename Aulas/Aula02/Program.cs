5965737454d16c619d36fa65655aa774c2039fb1
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-07 15:49:06 -0300

    Aula02 - Padrão dotnet

---
+++ Aulas/Aula02/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Aula02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.GetValue(0));
+        }
+    }
+}
+
