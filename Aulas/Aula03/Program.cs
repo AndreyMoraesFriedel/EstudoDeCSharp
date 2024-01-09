70f1497b9055b61cbdf72d3379392a982ebf069f
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-09 20:44:27 -0300

    Aula03 - Variaveis

---
+++ Aulas/Aula03/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program{
+    
+    static void Main(){
+        byte byt = 10; //0..255
+        int inteiro = 77777;
+        char cha = 'c';
+        float valorFloat = 9.50f;
+        string aprendendo = "Isto é uma String em C#";
+
+        var varDin = 10; //Em C# existe variavel dinamica var
+    
+        Console.WriteLine($"Byte: {byt}, Int: {inteiro}, Char: {cha}, Float: {valorFloat}, String: {aprendendo}, Var: {varDin}");
+    }
+
+}
