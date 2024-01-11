68e53df661ad39902fcae7188f980ec123fb662a
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-11 19:39:23 -0300

    Aula04 - EScopo de Variaveis

---
+++ Aulas/Aula04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula04{
+    class Program{
+    
+        static int num=10; //global
+        static void Main(){       
+            int num2=0; //local
+            Console.WriteLine(num); 
+        }
+    }
+}
