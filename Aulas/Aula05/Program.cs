a6163f933436a55bc66e2f1a190c55592a53c060
AndreyMoraesFriedel <AndreyMoraesFriedel@gmail.com>
2024-01-13 16:30:40 -0300

    Aula05 - Operadores e Operações

---
+++ Aulas/Aula05/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+
+    /*aula 5- operadores
+
+    Operadores Aritméticos
+
+    +	Adição
+    -	Subtração
+    *	Multiplicação
+    /	Divisão
+    %	Módulo (resto da divisão)
+
+    Operadores Aritméticos de Atribuição Reduzida
+
+    + =	mais igual
+    - =	menos igual
+    * =	vezes igual
+    / =	dividido igual
+    % =	módulo igual
+
+    Operadores Relacionais
+
+    ==	Igual a
+    !=	Diferente de
+    >	Maior que
+    <	Menor que
+    >=	Maior do que ou igual a
+    <=	Menor do que ou igual a
+
+    Operadores Lógicos
+
+    &&	AND = E
+    ||	OR = Ou
+    !	NOT = Não
+
+
+    */
+
+    bool res1 = (7>6) && (7==7);
+    Console.WriteLine(res1);
+
+    bool res2 = (9>6) || (2==7);
+    Console.WriteLine(res2);
+
+    /*
+    int res = (10 + 5) * 2;
+    Console.WriteLine(res);
+    */
+
+    /*
+    bool res = 10!=10;
+    Console.WriteLine(res);
+    */
+
+    /*
+    int res = 10;
+    res += 1; //res ++; res = res + 1; res +=1
+    Console.WriteLine(res);
+    */
+    
+    }
+
+}
