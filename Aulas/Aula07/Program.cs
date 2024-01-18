e83456e8301e37b9ba1ff43ee3f2353e751cf5dc
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-01-18 18:00:26 -0300

    Aula07 - Constantes

---
+++ Aulas/Aula07/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program{
+
+    static void Main(){
+
+        const string ALUNO = "Andrey";
+        const double PI = 3.1415;
+
+        Console.WriteLine("Aluno: {0}, numero PI: {1:N2}", ALUNO, PI);
+
+    }
+}
