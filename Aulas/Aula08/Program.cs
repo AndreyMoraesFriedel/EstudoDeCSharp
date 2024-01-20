2aad2ddd4d9a1558912bed81d96067a54da35c2b
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-01-20 17:05:14 -0300

    Aula08 - Lendo Valores do Teclado

---
+++ Aulas/Aula08/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        int primeiraNota; int segundaNota; 
+        double media;
+        string nomeEstudante;
+        
+        Console.Write("Digite nome do estudante: ");
+        nomeEstudante = Console.ReadLine();
+
+        Console.Write("Nota 1: ");
+        primeiraNota = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Nota 2: ");
+        segundaNota = Convert.ToInt32(Console.ReadLine());
+
+        media = (primeiraNota + segundaNota) / 2;
+
+        Console.WriteLine($"A media do estudante é {media}");
+        if(media>=7){
+            Console.WriteLine($"O aluno {nomeEstudante} esta Aprovado");
+        }else if (media>=5 && media <=6)
+        {
+            Console.WriteLine($"O aluno {nomeEstudante} esta de Recuperacao");
+        }else{
+            Console.WriteLine($"O aluno {nomeEstudante} esta Reprovado");
+        }
+
+    }
+}
