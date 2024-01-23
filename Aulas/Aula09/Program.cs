eaa9e40a624372458a3a726565eb4310794930b8
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-01-23 20:55:28 -0300

    Aula09 - Operações Bitwise

---
+++ Aulas/Aula09/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        //Operadores Bitwise
+        int num=10;
+        int num2=50;
+
+        num=num<<1; //Dobrando 10 = 20
+        num2=num2>>1; //Metade de 50 = 25
+
+        Console.WriteLine("O Valor do numero: {0}", num);
+        Console.WriteLine("O Valor do numero 2: {0}", num2);
+    }
+}
