5802c4cc143d8fc5fed48c529c0808ef2ac6e69c
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-02-01 11:18:05 -0300

    Aula10 - Enum

---
+++ Aulas/Aula10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program{
+
+    enum WeekDays{Sunday,Monday,Tuesday,
+    Wednesday,Thursday,Friday,
+    Saturday
+    };
+
+    static void Main(){
+
+        //WeekDays wD = WeekDays.Sunday;
+        //WeekDays wD = (WeekDays)3;
+
+        int wD = (int) Program.WeekDays.Tuesday; 
+
+
+        Console.WriteLine(wD);
+        
+    }
+}
