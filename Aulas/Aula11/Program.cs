bfb5f3e8f7b4684b3b040fdafdcf445e674950a0
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-02-01 18:09:22 -0300

    Aula11 - TypeCast

---
+++ Aulas/Aula11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+
+        //Exemplo de Conversão Implicita
+        int n1 = 10;
+        float n2 = n1;
+
+        Console.WriteLine(n2);
+
+        //Exemplo de Conversão Explícita
+        float n3 = 10.5f;
+        int n4 = (int) n3;
+
+        Console.WriteLine(n4);
+
+    }
+}
