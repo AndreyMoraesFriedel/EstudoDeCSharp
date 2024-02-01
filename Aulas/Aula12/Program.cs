e5fa019cc28495c7626b3a05ef431f4a7b8e7385
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-02-01 18:49:32 -0300

    Aula12 - Comando Condicional IF

---
+++ Aulas/Aula12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+
+        const int QUANTIDADE_NOTAS = 4;
+        float[] notasDoEstudante = new float[QUANTIDADE_NOTAS];
+        float media, somadorDeNotas;
+        media = somadorDeNotas = 0f;
+        string resultado = "";
+
+        for(int i = 0;i<QUANTIDADE_NOTAS;i++){
+            Console.Write($"Nota {i + 1}: ");
+            notasDoEstudante[i] = float.Parse(Console.ReadLine());
+            somadorDeNotas = notasDoEstudante[i];
+        }
+
+        media = somadorDeNotas / QUANTIDADE_NOTAS;
+        
+        if(media>=7.0){
+            resultado = "Passou";
+        }
+
+        Console.WriteLine($"Status do Aluno: {resultado}");
+
+    }
+}
