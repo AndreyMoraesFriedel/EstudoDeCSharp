3c7651a5f5d7533a6e607afe8dcfacd01c40e828
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-02-01 19:18:10 -0300

    Aula13 - IF - ELSE IF - ELSE

---
+++ Aulas/Aula13/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program{
+    static void Main(){
+        const int QUANTIDADE_NOTAS = 4;
+        int[] notasDoEstudante = new int[QUANTIDADE_NOTAS];
+        float media = 0f, somadorDeNotas = 0f;
+        string resultado = "";
+
+        for(int i = 0;i<QUANTIDADE_NOTAS;i++){
+            Console.Write($"Nota {i + 1}: ");
+            notasDoEstudante[i] = int.Parse(Console.ReadLine());
+            somadorDeNotas += notasDoEstudante[i];
+        }
+
+        media = somadorDeNotas / QUANTIDADE_NOTAS;
+        
+        if(media>=7.0){
+            resultado = "Passou";
+        }
+
+        else if(media>=5.5 && media<7){
+            resultado = "Recuperação";
+        }
+        
+        else{
+            resultado = "Reprovado";
+        }
+
+        Console.WriteLine($"Média: {media.ToString("F2")} | Status do Aluno: {resultado}");
+
+    }
+}
