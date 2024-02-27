1b68d40688238c7c646db1b01963e9cc49d2bc6f
AndreyMoraesFriedel <andreymoraesfriedel@gmail.com>
2024-02-26 21:42:42 -0300

    Aula17 - Array / Vetor

---
+++ Aulas/Aula17/Aula17/Aula17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+class Aula17
+{
+    static void Main()
+    {
+        int[] numeros = {55, 79, 423, 99, 20}; //0-4
+
+        foreach (int x in numeros)
+        {
+            Console.WriteLine(x);  
+        }
+    }
+}
